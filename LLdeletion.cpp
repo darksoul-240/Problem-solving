b59081db56781642f4c818e8c69aca4768aa4778
darksoul-240 <ibrahim28isa@gmail.com>
2025-06-24 14:05:58 +0530

    Refer to this

---
+++ LLdeletion.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    struct ListNode {
+      int val;
+      ListNode* next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode* next) : val(x), next(next) {}
+  };
+     ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            if (curr->val == val) {
+                prev->next = curr->next;
+                delete curr;
+                curr = prev->next; // move forward after delete
+            } else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return dummy.next;
+    }
+    
+};
