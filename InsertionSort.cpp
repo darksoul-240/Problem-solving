bf4b825647e514d41999efa2bf24cfe8f0f405db
darksoul-240 <ibrahim28isa@gmail.com>
2025-07-14 23:43:57 +0530

    Insertion Sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -6,11 +6,14 @@ public:
     vector<int> InsertionSort(vector<int>&nums){
         int n=nums.size();
         for(int i=0;i<n;i++){
-            for(int j=i;j>0;j--){
-                if(nums[i]>nums[j])swap(nums[i],nums[j]);
+            int key=nums[i];
+            int j=i-1;
+            while(j>=0&& nums[j]>key){
+                nums[j+1]=key;
+                j--;
             }
+            nums[j+1]=key;
         }
+        return nums;
     }
-   
-   
 };
