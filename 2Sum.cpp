c168b9e621b5033a7d069ee41406249594a06926
darksoul-240 <ibrahim28isa@gmail.com>
2025-07-16 21:52:34 +0530

    Using unordered maps

---
+++ 2Sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:    
+    vector<int>twoSum(vector<int>nums, int target){
+        vector<int>result;
+        int n=nums.size();
+        unordered_map<int,int>val_idx;
+        for(int i=0;i<n;i++){
+            int tar=target-nums[i];
+            if(val_idx.find(tar)!=val_idx.end()){
+                return {i,val_idx[tar]};
+            }
+            val_idx.insert({nums[i],i});
+        }
+        return {};
+
+    }
+};
